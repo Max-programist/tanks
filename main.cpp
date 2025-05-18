c1d125be887f6f973a7a00a62e27df7e0dd03713
Max <makskiller1511@gmail.com>
2025-05-18 21:10:36 +0300

    create border, map

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-//map 20*20
+//map 20*20 -------->
 //target
 //tank
 //tank move
@@ -11,19 +11,53 @@
 
 
 
+
+
+
+
 std::vector<std::vector<char>> create() {
     std::vector<std::vector<char>> arr = {};
     for(int i = 0; i < 20; i++) {
+        arr.push_back(std::vector<char>());
         for(int j = 0; j < 20; j++) {
-            // arr[i][j] = '.'; bug
+            arr[i].push_back('.');
         }
     }
     return arr;
 }
 
+// 1 5 9
+// 7 3 2
+// 0 4 6
+
+
+void print(const std::vector<std::vector<char>>& arr) {
+    
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+}
+
+
+void border(std::vector<std::vector<char>>& arr) {
+    for(int i = 0; i < 20; i++) {
+        for(int j = 0; j < 20; j++) {
+            if(i == 0 || i == 19 || j == 0 || j == 19)  {
+                arr[i][j] = '#';
+           }
+        }
+    }
+}
+
+
+
 int main() {
     std::vector<std::vector<char>> arr = create();
-    std::cout << "Hello world" << std::endl;
-
+    border(arr);
+    print(arr);
     return 0;
 }
